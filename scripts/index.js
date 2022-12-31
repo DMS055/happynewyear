ad6a725edfdfc28a68588699eec28787b1c3520b
DMS055 <thebest755p@gmail.com>
2022-12-31 01:01:46 +0100

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const iw = window.innerWidth;
+const ih = window.innerHeight;
+
+const loop = () => {
+
+}
+
+(() => {
+
+})();
