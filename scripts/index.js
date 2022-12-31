1c3f1b46bca5f95446ccb1a38a739d0b52ec79e7
DMS055 <thebest755p@gmail.com>
2022-12-31 01:52:54 +0100

    Main functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import { random } from "./utilities";
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -12,7 +14,17 @@ let tick = 0;
 
 const loop = () => {
     window.requestAnimationFrame(loop);
-}
+
+    hue = random(0, 360);
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.fillRect(0, 0, cw, ch);
+    ctx.globalCompositeOperation ='lighter';
+
+    for (let i = fireworks.length; i > 0; i--) {
+        fireworks[i].draw();
+    }
+};
 
 (() => {
     canvas.width = cw;
