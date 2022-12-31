5f13c05f191f40adabcbbf8af75cd0d9522b7666
DMS055 <thebest755p@gmail.com>
2022-12-31 02:20:55 +0100

    Create particle util

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,3 +1,5 @@
+import { Particle } from "./particle";
+
 const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -9,4 +11,11 @@ const calcDistance = (p1x, p1y, p2x, p2y) => {
     return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
-export { random, calcDistance };
+const createParticles = (x, y, hue, ctx, particles) => {
+    let particleCount = 30;
+    while(particleCount--) {
+        particles.push(new Particle(x, y, hue, ctx, particles));
+    }
+}
+
+export { random, calcDistance, createParticles };
